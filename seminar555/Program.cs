88635603e1bac525cc4ce8d25af24dd101cfe4eb
Oksana <avalonv264@gmail.com>
2022-12-03 13:44:12 +0300

    task2

---
+++ seminar555/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+int n, m;
+Console.WriteLine("Ведите два числа: ");
+n = Convert.ToInt32(Console.ReadLine());
+m = Convert.ToInt32(Console.ReadLine());
+if (n > m)
+{
+    Console.WriteLine("{0} наибольшее число", n);
+}
+else
+{
+    Console.WriteLine ("{0} наибольшее число", m);
+}
+Console.ReadLine();
