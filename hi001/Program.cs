f904b966fad0fdd861748e929369c878718e8fea
Oksana <avalonv264@gmail.com>
2022-12-03 14:28:03 +0300

    task4

---
+++ hi001/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+int a, b, c;
+Console.WriteLine("Ведите три числа: ");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+c = Convert.ToInt32(Console.ReadLine());
+if (a > b && a > c);
+{
+    Console.WriteLine("{0} наибольшее число", a);
+}
+if (b > a && b > c);
+{
+    Console.WriteLine("{0} наибольшее число", b);
+}
+if (c > a && c > b);
+{
+    Console.WriteLine("{0} наибольшее число", c);
+}
+Console.ReadLine();
